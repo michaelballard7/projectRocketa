55378f767a3428ceaeda5b3e83d6af2606a865e6
Michael Ballard <ballard.michael9@gmail.com>
2017-03-12 12:27:58 -0400

    Initial Commit

---
+++ projectRocketa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  projectRocketa
+//
+//  Created by Michael Ballard on 3/12/17.
+//  Copyright © 2017 Michael Ballard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
