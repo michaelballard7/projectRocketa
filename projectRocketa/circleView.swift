60f56c85902142e4c2baef4d6b6d29e4e7016b87
Michael Ballard <ballard.michael9@gmail.com>
2017-03-25 12:28:58 -0400

    added assets

---
+++ projectRocketa/circleView.swift
@@ -0,0 +1,18 @@
+//
+//  circleView.swift
+//  projectRocketa
+//
+//  Created by Michael Ballard on 3/25/17.
+//  Copyright © 2017 Michael Ballard. All rights reserved.
+//
+
+import UIKit
+
+class circleView: UIImageView {
+
+    override func layoutSubviews() {
+        
+    
+    }
+
+}
